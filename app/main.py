267b3fc54718ef0f5822e2252a36117d000e1b61
William Kral <william.kral@gmail.com>
2012-03-24 13:05:21 -0700

    Adding in base template

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Find a store"
+    return render_template('index.html')
